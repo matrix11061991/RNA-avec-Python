2d3e89743ac0f9e96d51195a706f61e1b9412e90
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2022-12-12 07:36:30 +0300

    Add files via upload

---
+++ RNA-Keras-Exemple/RNA-Keras-Ex2.py
@@ -0,0 +1,23 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Définition des entrées du réseau
+
+x = np.array([[1, 2], [3, 4], [5, 6]])
+
+# Création du modèle de réseau de neurones en utilisant la bibliothèque Keras
+
+model = Sequential()
+model.add(Dense(4, input_dim=2, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compilation et entraînement du modèle en utilisant l'algorithme de descente de gradient
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(x, y, epochs=10, batch_size=1)
+
+# Évaluation du modèle sur des données de test
+
+scores = model.evaluate(x, y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
